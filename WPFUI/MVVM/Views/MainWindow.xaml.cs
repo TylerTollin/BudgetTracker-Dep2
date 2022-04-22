926f433e3a3158ccb158c2ed8cb8322c95ffe96f
Tyler Tollin <tyler.tollin@gmail.com>
2022-04-22 17:24:34 -0500

    Moved MainWindow into Views

--- WPFUI/MVVM/Views/MainWindow.xaml.cs
+++ WPFUI/MVVM/Views/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WPFUI.MVVM.Views;
 
 namespace WPFUI
 {
@@ -20,9 +21,37 @@ namespace WPFUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        HomeUI homeUI = new HomeUI();
+        IncomeUI incomeUI = new IncomeUI();
+        ExpensesUI expensesUI = new ExpensesUI();   
+
         public MainWindow()
         {
             InitializeComponent();
+            homeUI.Visibility = Visibility.Visible;
+            incomeUI.Visibility = Visibility.Hidden;
+            expensesUI.Visibility = Visibility.Hidden;
+        }
+
+        private void radioHome_Click(object sender, RoutedEventArgs e)
+        {
+            homeUI.Visibility = Visibility.Visible;
+            incomeUI.Visibility = Visibility.Hidden;
+            expensesUI.Visibility = Visibility.Hidden;
+        }
+
+        private void radioIncome_Click(object sender, RoutedEventArgs e)
+        {
+            homeUI.Visibility = Visibility.Hidden;
+            incomeUI.Visibility = Visibility.Visible;
+            expensesUI.Visibility = Visibility.Hidden;
+        }
+
+        private void radioExpenses_Click(object sender, RoutedEventArgs e)
+        {
+            homeUI.Visibility = Visibility.Hidden;
+            incomeUI.Visibility = Visibility.Hidden;
+            expensesUI.Visibility = Visibility.Visible;
         }
     }
 }
